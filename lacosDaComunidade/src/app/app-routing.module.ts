a85ae4fa9576f6ecd78082050592115ad5a8f666
FelipeFFS93 <felipe_freiresilva@yahoo.com.br>
2021-08-26 13:15:53 -0300

    Atualização da parte front do projeto em Angular

--- lacosDaComunidade/src/app/app-routing.module.ts
+++ lacosDaComunidade/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"", redirectTo: "menu", pathMatch: "full"},
+
+  {path:"entrar", component:EntrarComponent},
+  {path: "cadastrar", component:CadastrarComponent},
+  {path: "menu", component:MenuComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

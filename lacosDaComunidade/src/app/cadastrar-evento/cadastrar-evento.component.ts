9c8cc8d8bb216e84af8220071a972b2913458bd0
bigboss2021 <limavalmir2000@hotmail.com>
2021-09-21 12:58:20 -0300

    eventos/implementação

--- lacosDaComunidade/src/app/cadastrar-evento/cadastrar-evento.component.ts
+++ lacosDaComunidade/src/app/cadastrar-evento/cadastrar-evento.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/tema';
+import { TemaService } from '../service/tema.service';
 
 
 @Component({
@@ -8,11 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarEventoComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private temaService: TemaService
+  ) { }
 
   ngOnInit() {
-    window.scroll(0,0)
+    if (environment.token == '') {
+      this.router.navigate(['/home'])
+    }
+    this.findAllTemas()
   }
+  
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+
+  cadastrar() {
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
+  }
+
+
 
 }
